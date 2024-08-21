17eed2bf245e1c55b63df17c79e0ceb87d2d3151
DanielGRasmussen <104102130+DanielGRasmussen@users.noreply.github.com>
2024-08-21 14:58:51 -0700

    Added sticky header logic

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,11 @@
-import { Component, ElementRef, OnInit, QueryList, ViewChildren } from "@angular/core";
+import {
+	Component,
+	ElementRef,
+	HostListener,
+	OnInit,
+	QueryList,
+	ViewChildren,
+} from "@angular/core";
 import { ThemeToggleComponent } from "./theme-toggle/theme-toggle.component";
 import { RouterLink, RouterLinkActive, Router } from "@angular/router";
 import { NgForOf } from "@angular/common";
@@ -12,7 +19,11 @@ import { NgForOf } from "@angular/common";
 })
 export class HeaderComponent implements OnInit {
 	@ViewChildren("link") linkElements!: QueryList<ElementRef>;
+	activeIndex: number = 0;
+	isHidden: boolean = false;
+	private previousScrollPosition: number = 0;
 
+	// Creates the links in the header
 	links: { name: string; url: string }[] = [
 		{ name: "Home", url: "/home" },
 		{ name: "Projects", url: "/projects" },
@@ -21,8 +32,6 @@ export class HeaderComponent implements OnInit {
 		{ name: "Contact Info", url: "/contact" },
 	];
 
-	activeIndex: number = 0;
-
 	constructor(private router: Router) {}
 
 	ngOnInit(): void {
@@ -30,22 +39,31 @@ export class HeaderComponent implements OnInit {
 		this.router.events.subscribe(this.updateActiveIndex.bind(this));
 	}
 
+	// Underline logic
 	updateActiveIndex(): void {
 		this.activeIndex = this.links.findIndex(link => this.router.url.startsWith(link.url));
 	}
 
+	getNativeElement(index: number): HTMLElement | null {
+		if (!this.linkElements || this.linkElements.length === 0) return null;
+		const element = this.linkElements.toArray()[index];
+		return element ? element.nativeElement : null;
+	}
+
 	getUnderlineOffset(): string {
-		if (!this.linkElements || this.linkElements.length === 0) return "0";
-		const activeElement = this.linkElements.toArray()[this.activeIndex].nativeElement;
+		const activeElement: HTMLElement | null = this.getNativeElement(this.activeIndex);
+		if (!activeElement) return "0";
+
 		// Get offset from left of the active element with left padding of the element
 		const style: CSSStyleDeclaration = window.getComputedStyle(activeElement);
-		const offset = activeElement.offsetLeft + parseFloat(style.paddingLeft);
+		const offset: number = activeElement.offsetLeft + parseFloat(style.paddingLeft);
 		return offset + "px";
 	}
 
 	getUnderlineWidth(): string {
-		if (!this.linkElements || this.linkElements.length === 0) return "0";
-		const activeElement = this.linkElements.toArray()[this.activeIndex].nativeElement;
+		const activeElement: HTMLElement | null = this.getNativeElement(this.activeIndex);
+		if (!activeElement) return "0";
+
 		// Get width without padding
 		const style: CSSStyleDeclaration = window.getComputedStyle(activeElement);
 		const width: number =
@@ -56,8 +74,42 @@ export class HeaderComponent implements OnInit {
 	}
 
 	getUnderlineBottom(): string {
-		if (!this.linkElements || this.linkElements.length === 0) return "0";
-		const activeElement = this.linkElements.toArray()[this.activeIndex].nativeElement;
+		const activeElement: HTMLElement | null = this.getNativeElement(this.activeIndex);
+		if (!activeElement) return "0";
+
 		return activeElement.offsetHeight - 34 + "px";
+	}
+
+	// Sticky logic
+	@HostListener("window:scroll")
+	onWindowScroll(): void {
+		this.handleStickyHeader();
+	}
+
+	handleStickyHeader(): void {
+		const currentScrollPosition: number = window.scrollY || document.documentElement.scrollTop;
+
+		// Get header height
+		const header: HTMLElement | null = document.querySelector("header");
+		if (!header) return;
+		const headerHeight: number = header.offsetHeight;
+
+		// If the scroll height is less than the header height, show the header
+		if (currentScrollPosition < headerHeight) {
+			this.isHidden = false;
+			this.previousScrollPosition = currentScrollPosition;
+			return;
+		}
+
+		// If the scroll is going up, show the header
+		if (currentScrollPosition < this.previousScrollPosition - 10) {
+			this.isHidden = false;
+			this.previousScrollPosition = currentScrollPosition;
+			return;
+		} else if (currentScrollPosition > this.previousScrollPosition + 10) {
+			this.isHidden = true;
+			this.previousScrollPosition = currentScrollPosition;
+			return;
+		}
 	}
 }
