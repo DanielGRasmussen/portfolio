059c1ba992cebc5660d68d4c16a4a18cd92f302f
DanielGRasmussen <104102130+DanielGRasmussen@users.noreply.github.com>
2025-03-10 23:45:47 -0700

    Split the image viewer into its own component.

--- src/app/modules/projects/project-view/project-view.component.ts
+++ src/app/modules/projects/project-view/project-view.component.ts
@@ -6,11 +6,12 @@ import { NgForOf, NgIf } from "@angular/common";
 import { ProjectInfoComponent } from "../project-info/project-info.component";
 import { AccordionComponent } from "./acordian/accordion.component";
 import { TitleService } from "../../../title.service";
+import { ImageViewerComponent } from "./image-viewer/image-viewer.component";
 
 @Component({
 	selector: "app-project-view",
 	standalone: true,
-	imports: [NgForOf, ProjectInfoComponent, AccordionComponent, NgIf],
+	imports: [NgForOf, ProjectInfoComponent, AccordionComponent, NgIf, ImageViewerComponent],
 	templateUrl: "./project-view.component.html",
 	styleUrl: "./project-view.component.scss",
 })
@@ -19,12 +20,6 @@ export class ProjectViewComponent implements OnInit {
 	route: ActivatedRoute = inject(ActivatedRoute);
 	id: string;
 	images: string[] = [];
-
-	currentIndex: number = 0;
-
-	useOverlay: boolean = false;
-	closingOverlay: boolean = false;
-	overlayClickStarted: boolean = false;
 
 	constructor(
 		private ContentService: ContentService,
@@ -51,77 +46,5 @@ export class ProjectViewComponent implements OnInit {
 
 	ngOnInit(): void {
 		this.TitleService.setTitle("Projects - " + this.project.title);
-	}
-
-	goToImage(index: number): void {
-		if (index < 0 || index >= this.images.length) {
-			return;
-		}
-
-		this.currentIndex = index;
-	}
-
-	nextImage(): void {
-		if (this.currentIndex < this.images.length - 1) {
-			this.currentIndex++;
-		}
-	}
-
-	previousImage(): void {
-		if (this.currentIndex > 0) {
-			this.currentIndex--;
-		}
-	}
-
-	getActive(type: "next" | "previous"): boolean {
-		if (type === "next") {
-			return !(this.currentIndex >= this.images.length - 1);
-		}
-		return this.currentIndex > 0;
-	}
-
-	keybindings(event: KeyboardEvent): void {
-		if (!this.useOverlay) {
-			return;
-		}
-		if (event.key === "ArrowRight") {
-			this.nextImage();
-		} else if (event.key === "ArrowLeft") {
-			this.previousImage();
-		} else if (event.key === "Escape") {
-			this.closeOverlay();
-		}
-		event.stopPropagation();
-		event.preventDefault();
-	}
-
-	onOverlayMouseDown(event: MouseEvent): void {
-		// Check if the event target is the overlay container itself
-		if (event.target === event.currentTarget) {
-			this.overlayClickStarted = true;
-		}
-	}
-
-	onOverlayMouseUp(event: MouseEvent): void {
-		// Check that the mouseup is on the overlay and that the click started on the overlay
-		if (event.target === event.currentTarget && this.overlayClickStarted) {
-			this.closeOverlay();
-		}
-
-		this.overlayClickStarted = false;
-	}
-
-	openOverlay(): void {
-		this.useOverlay = true;
-		// Add overlay keybindings
-		document.addEventListener("keydown", this.keybindings.bind(this));
-	}
-
-	closeOverlay(): void {
-		this.closingOverlay = true;
-		setTimeout(() => {
-			this.useOverlay = false;
-			this.closingOverlay = false;
-		}, 200);
 	}
 }
